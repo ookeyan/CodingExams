4a7511801c0fe46c1f59b8347c39da202fbc0d97
Qadira Moore (RIT Student) <qm6148@rit.edu>
2024-10-16 23:14:29 -0400

    10.16.24

---
+++ two_pointers/remove_element.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def removeElement(nums: List[int], val: int) -> int:
+    index = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[index] = nums[i]
+            index += 1
+    return index , nums
+
+nums = [3,2,2,3]
+val =3
+print(removeElement(nums, val))
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+print(removeElement(nums, val))
